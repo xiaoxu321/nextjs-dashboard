8189cebe0861ca72324004a4c315a8659a86187a
bert <bert.xu@facilityone.cn>
2024-05-09 14:27:07 +0800

    feat:example

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function ddd() {
+    return <p>Customers Page</p>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+      <div>111</div>
+      {children}
+      </body>
     </html>
   );
 }
